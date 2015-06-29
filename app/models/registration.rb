402b709a7b6b8db9ad6ef6d0ffdd655086a7dc90
Le Anh <le.ngoc.anh@framgia.com>
2015-06-29 15:12:12 +0700

    first paypal basics

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -6,19 +6,38 @@ class Registration < ActiveRecord::Base
   serialize :notification_params, Hash
   def paypal_url(return_path)
     values = {
-        business: "merchant@gotealeaf.com",
-        cmd: "_xclick",
-        upload: 1,
-        return: "#{Rails.application.secrets.app_host}#{return_path}",
-        invoice: id,
-        amount: course.price,
-        item_name: course.name,
-        item_number: course.id,
-        quantity: '1',
-        notify_url: "#{Rails.application.secrets.app_host}/hook"
+      business: "leanh@merchant.com",
+      cmd: "_xclick",
+      upload: 1,
+      return: "#{Settings.app_host}#{return_path}",
+      invoice: id,
+      amount: course.price,
+      item_name: course.name,
+      item_number: course.id,
+      quantity: '3',
+      notify_url: "#{Settings.app_host}/hook"
     }
-    "#{Rails.application.secrets.paypal_host}/cgi-bin/webscr?" + values.to_query
+    "#{Settings.paypal_host}/cgi-bin/webscr?" + values.to_query
   end
 
-
+  # test paying multi item
+  # def paypal_url(return_path)
+  #   values = {
+  #     business: "leanh@merchant.com",
+  #     cmd: "_cart",
+  #     upload: 1,
+  #     return: "#{Settings.app_host}#{return_path}",
+  #     invoice: id,
+  #     amount_1: 100,
+  #     item_name_1: "item no 01",
+  #     item_number_1: 1,
+  #     quantity_1: "1",
+  #     amount_2: 200,
+  #     item_name_2: "item no 02",
+  #     item_number_2: 2,
+  #     quantity_2: "2",
+  #     notify_url: "#{Settings.app_host}/hook"
+  #   }
+  #   "#{Settings.paypal_host}/cgi-bin/webscr?" + values.to_query
+  # end
 end
